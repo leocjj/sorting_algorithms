7067c22dcf44ed0bd5b2e9b85c91e379028d1e36
leocjj <leocj@hotmail.com>
2020-02-03 14:35:58 -0500

    Task 04

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -16,8 +16,8 @@ void shell_sort(int *array, size_t size)
 		return;
 
 	gap = 1;
-	/* Start with the largest gap and work down to a gap of 1 */
-	/* 3x+1 increment sequence:  1, 4, 13, 40, 121, 364, 1093, ... */
+	/* Start with the largest gap and work down to a gap of 1*/
+	/* 3x+1 increment sequence:  1, 4, 13, 40, 121, 364, 1093, ...*/
 	while (gap < size / 3)
 		gap = 3 * gap + 1;
 
