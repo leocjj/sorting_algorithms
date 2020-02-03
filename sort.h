93717f7d70a682c5e599a0feef0469708e8cea89
leocjj <leocj@hotmail.com>
2020-02-03 10:06:20 -0500

    Header file updated

--- sort.h
+++ sort.h
@@ -34,7 +34,7 @@ void double_swap(listint_t *A, listint_t *B);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void q_sort(int *array, int first,int last);
+void q_sort(int *array, int first, int last);
 
 
 void bubble_sort_back(int *array, size_t size);
