d7375a90333a71dad86d41c80a92acfce7562860
leocjj <leocj@hotmail.com>
2020-02-03 13:09:03 -0500

    Header file updated

--- sort.h
+++ sort.h
@@ -30,12 +30,25 @@ void print_list(const listint_t *list);
 
 void simple_swap(int *first, int *second);
 void bubble_sort(int *array, size_t size);
+
 void double_swap(listint_t *A, listint_t *B);
 void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
-void q_sort(int a[], int l, int h, size_t s);
 
-void bubble_sort_back(int *array, size_t size);
+void selection_sort(int *array, size_t size);
+
+int partition(int a[], int l, int h, size_t s);
+void q_sort(int a[], int l, int h, size_t s);
+void quick_sort(int *array, size_t size);
+
+void d_l_list_swap_n(listint_t *a, listint_t *b, listint_t **list);
+void cocktail_sort_list(listint_t **list);
+
+void m_sort(int *a, int l, int m, int r, int *tmp);
+void m_d_g(int *a, int l, int r, int *tmp);
+void r_sort(int *a, int s, int e);
+int find_biggest(int *a, size_t s);
+
+void merge_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
 
 #endif /*SORT_H_ */
