c15bbd7d829b61a4b4c7b96e937a0170c83bb683
leocjj <leocj@hotmail.com>
2020-02-03 10:22:03 -0500

    Task 02

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -23,22 +23,23 @@ void simple_swap(int *first, int *second)
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, jMin;
-    /* advance the position through the entire array */
-    /*   (could do i < aLength-1 because single element is also min element) */
-    for (i = 0; i < size-1; i++)
-    {
-        /* find the min element in the unsorted a[i .. aLength-1] */
-        /* assume the min is the first element */
-        jMin = i;
-        /* test against elements after i to find the smallest */
-        for (j = i+1; j < size; j++)
-            if (array[j] < array[jMin]) /* if this element is less, then it is the new minimum */
-                jMin = j;   /* found new minimum; remember its index */
-        if (jMin != i)
-        {
-            simple_swap(&array[i], &array[jMin]);
-            print_array(array, size);
-        }
-    }
+	size_t i, j, jMin;
+	/* advance the position through the entire array */
+	/*   (could do i < aLength-1 because single element is also min element) */
+	for (i = 0; i < size - 1; i++)
+	{
+		/* find the min element in the unsorted a[i .. aLength-1] */
+		/* assume the min is the first element */
+		jMin = i;
+		/* test against elements after i to find the smallest */
+		for (j = i + 1; j < size; j++)
+			/* if this element is less, then it is the new minimum */
+			if (array[j] < array[jMin])
+				jMin = j;   /* found new minimum; remember its index */
+		if (jMin != i)
+		{
+			simple_swap(&array[i], &array[jMin]);
+			print_array(array, size);
+		}
+	}
 }
